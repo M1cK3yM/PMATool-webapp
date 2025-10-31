c6670ee755ae48a43fb9b9153c119fed2fe82a65
Yodahe Zegeye <yodahemesay@gmail.com>
2025-10-31 17:08:30 -0400

    Changed port number in api-client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = process.env.NEXT_PUBLIC_API_DOMAIN || "http://localhost:8080"
+const baseURL = process.env.NEXT_PUBLIC_API_DOMAIN || "http://localhost:4444"
 
 export const apiClient = axios.create({
   baseURL,
